a78d8cc71b714667063cbfd80dcb634268d5dd46
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-11 04:35:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
-#write your code here
+def countdown(num)
+  while num > 0
+   sleep(1) 
+    puts"#{num}SECONDS(S)!"      
+      num -= 1 
+      
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  
 
-def countdown
+def countdown_with_sleep(num)
+  while num > 0
+  puts "#{num}SECONDS(S)!"
+  sleep(1)
+  num -= 1 
+  
 end
+puts  "HAPPY NEW YEAR!"
+  end
+
+ 
+ 
